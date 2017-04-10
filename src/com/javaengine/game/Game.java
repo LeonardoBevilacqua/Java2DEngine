2db41df40b26338e0e776bfcc1e8ad6c90367573
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2017-04-10 14:24:02 -0300

    5 aula

--- src/com/javaengine/game/Game.java
+++ src/com/javaengine/game/Game.java
@@ -1,5 +1,6 @@
 package com.javaengine.game;
 
+import com.javaengine.game.gfx.Colours;
 import com.javaengine.game.gfx.Screen;
 import com.javaengine.game.gfx.SpriteSheet;
 import java.awt.BorderLayout;
@@ -26,6 +27,7 @@ public class Game extends Canvas implements Runnable{
     
     private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
     private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+    private int[] colours = new int[6*6*6];
     
     private Screen screen;
     public InputHandler input;
@@ -49,6 +51,19 @@ public class Game extends Canvas implements Runnable{
     }
     
     public void init(){
+        int index = 0;
+        for (int r = 0; r < 6; r++) { // reds
+            for (int g = 0; g < 6; g++) { // greens
+                for (int b = 0; b < 6; b++) { // blues
+                    int rr = (r * 255/5); // red
+                    int gg = (g * 255/5); // green
+                    int bb = (b * 255/5); // blue
+                    
+                    colours[index++] = rr << 16 | gg << 8 | bb;
+                }
+            }
+        }
+        
         screen = new Screen(WIDTH, HEIGHT, new SpriteSheet("/sprite_sheet.png"));
         input = new InputHandler(this);
     }
@@ -126,7 +141,19 @@ public class Game extends Canvas implements Runnable{
             return;
         }
         
-        screen.render(pixels, 0, WIDTH);
+        for (int y = 0; y < 32; y++) {
+            for (int x = 0; x < 32; x++) {
+                screen.render(x << 3, y << 3, 0, Colours.get(555, 500, 050, 005));
+            }
+        }
+        
+        
+        for (int y = 0; y < screen.height; y++) {
+            for (int x = 0; x < screen.width; x++) {
+                int colourCode = screen.pixels[x + y * screen.width];
+                if (colourCode < 255) pixels[x + y * WIDTH] = colours[colourCode];
+            }
+        }
         
         Graphics g = bs.getDrawGraphics();
         
