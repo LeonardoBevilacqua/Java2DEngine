158c1a5814a5b6e8d8d0a447397d393d3a198f27
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2017-04-10 13:26:59 -0300

    4 aula

--- src/com/javaengine/game/Game.java
+++ src/com/javaengine/game/Game.java
@@ -28,6 +28,7 @@ public class Game extends Canvas implements Runnable{
     private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
     
     private Screen screen;
+    public InputHandler input;
     
     public Game(){
         setMinimumSize(new Dimension(WIDTH*SCALE, HEIGHT * SCALE));
@@ -49,6 +50,7 @@ public class Game extends Canvas implements Runnable{
     
     public void init(){
         screen = new Screen(WIDTH, HEIGHT, new SpriteSheet("/sprite_sheet.png"));
+        input = new InputHandler(this);
     }
     
     public synchronized void start() {
@@ -109,8 +111,11 @@ public class Game extends Canvas implements Runnable{
     // update the logic of the game
     public void tick(){
         tickCount++;
-        screen.xOffset++;
-        screen.yOffset++;
+        
+        if (input.up.isPressed()) screen.yOffset--;
+        if (input.down.isPressed()) screen.yOffset++;
+        if (input.left.isPressed()) screen.xOffset--;
+        if (input.right.isPressed()) screen.xOffset++;
         
     }
     // update the visual of the game
