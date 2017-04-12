2e1632c10a31c37c793de5274299f6af9f65444f
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2017-04-12 13:28:31 -0300

    12 aula

--- src/com/javaengine/game/entities/Mob.java
+++ src/com/javaengine/game/entities/Mob.java
@@ -1,7 +1,7 @@
 package com.javaengine.game.entities;
 
-import com.javaengine.game.gfx.Screen;
 import com.javaengine.game.level.Level;
+import com.javaengine.game.level.tiles.Tile;
 
 public abstract class Mob extends Entity{
     
@@ -40,6 +40,15 @@ public abstract class Mob extends Entity{
     }
     
     public abstract boolean hasCollided(int xa, int ya);
+    
+    protected boolean isSolidTile(int xa, int ya, int x, int y){
+        if (level == null) return false;
+        
+        Tile lastTile = level.getTile((this.x + x) >> 3, (this.y + y) >> 3);
+        Tile newTile = level.getTile((this.x + x + xa) >> 3, (this.y + y + ya) >> 3);
+        
+        return !lastTile.equals(newTile) && newTile.isSolid();
+    }
     
     public String getName(){
         return name;
