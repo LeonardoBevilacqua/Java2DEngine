a38fdef1d96455c659bc1b7f0931357a1fe753f3
Leonardo Bevilacqua <leonardo_bevilacqua@hotmail.com>
2017-04-13 10:56:37 -0300

    14 aula

--- src/com/javaengine/game/level/tiles/Tile.java
+++ src/com/javaengine/game/level/tiles/Tile.java
@@ -4,44 +4,49 @@ import com.javaengine.game.gfx.Colours;
 import com.javaengine.game.gfx.Screen;
 import com.javaengine.game.level.Level;
 
-
 public abstract class Tile {
-    
+
     public static final Tile[] tiles = new Tile[256];
     public static final Tile VOID = new BasicSolidTile(0, 0, 0, Colours.get(000, -1, -1, -1), 0xFF000000);
     public static final Tile STONE = new BasicSolidTile(1, 1, 0, Colours.get(-1, 333, -1, -1), 0xFF555555);
     public static final Tile GRASS = new BasicTile(2, 2, 0, Colours.get(-1, 131, 141, -1), 0xFF00FF00);
-    
+    public static final Tile WATER = new AnimatedTile(3, new int[][]{{0, 5}, {1, 5}, {2, 5}, {1, 5}}, Colours.get(-1, 004, 115, -1), 0xFF0000FF, 1000);
+    public static final Tile LAVA = new BasicTile(4, 3, 0, Colours.get(-1, 500, 541, -1), 0xFFFF0000);
+
     protected byte id;
     protected boolean solid;
     protected boolean emitter;
     private int levelColour;
-    
-    public Tile(int id, boolean isSolid, boolean isEmitter, int levelColour){
+
+    public Tile(int id, boolean isSolid, boolean isEmitter, int levelColour) {
         this.id = (byte) id;
-        if (tiles[id] != null) throw new RuntimeException("Duplicate tile id on " + id);
-        
+        if (tiles[id] != null) {
+            throw new RuntimeException("Duplicate tile id on " + id);
+        }
+
         this.solid = isSolid;
         this.emitter = isEmitter;
         this.levelColour = levelColour;
         tiles[id] = this;
     }
-    
-    public byte getId(){
+
+    public byte getId() {
         return id;
     }
-    
-    public boolean isSolid(){
+
+    public boolean isSolid() {
         return solid;
     }
-    
-    public boolean isEmitter(){
+
+    public boolean isEmitter() {
         return emitter;
     }
-    
-    public int getLevelColour(){
+
+    public int getLevelColour() {
         return levelColour;
     }
-    
+
+    public abstract void tick();
+
     public abstract void render(Screen screen, Level level, int x, int y);
 }
